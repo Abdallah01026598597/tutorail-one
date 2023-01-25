522704b65c528552089828e429fe30178b3ce4ea
abdallah elsayed <abdoalsayed4456@gmail.com>
2023-01-25 10:34:33 +0200

    angular task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RewardComponent } from './reward/reward.component';
+import { RightbarComponent } from './rightbar/rightbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    RewardComponent,
+    RightbarComponent
   ],
   imports: [
     BrowserModule,
